e29c91a55e503ac14d71250e8ba9d1a7150e9259
anyric <anyamaronyango@gmail.com>
2017-07-03 23:28:41 +0300

    update test file by adding more test cases

--- test.py
+++ test.py
@@ -11,10 +11,18 @@ class TestCheckisPrime(unittest.TestCase):
     def test_check7isprime(self):
         """ test to assert 7 is prime """
         self.assertEqual(prime.generate_prime_numbers(7), 7)
+        
+    def test_check11isprime(self):
+        """ test to assert 11 is not prime """
+        self.assertEqual(prime.generate_prime_numbers(11), 11)
 
     def test_check9isnotprime(self):
         """ test to assert 9 is not prime """
         self.assertEqual(prime.generate_prime_numbers(9), 'None')
+        
+    def test_check12isnotprime(self):
+        """ test to assert 12 is not prime """
+        self.assertEqual(prime.generate_prime_numbers(12), 'None')
 
 if __name__ == '__main__':
     unittest.main()
